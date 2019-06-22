1cb2692ed31a48f6069180088d40d19c37a222c1
kampkelly <kampkellykeys@gmail.com>
2019-06-22 23:12:47 +0100

    ft(setup-vue-project): setup vue application
    - refactor folder structure
    - setup vue project from vue cli
    
    [Finishes #hADRLjBA]

--- server/app_config.py
+++ server/app_config.py
@@ -2,12 +2,14 @@ import os
 from flask import Flask
 from flask_json import FlaskJSON, json_response
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 from config import config
 
 
 def create_app(config_name):
     app = Flask(__name__)
+    CORS(app)
     FlaskJSON(app)
     app.config.from_object(config[config_name])
 
